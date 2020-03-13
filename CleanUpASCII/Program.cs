5c926eeccda8a91cc5c452868a28e99b8871a00f
János Szomor <jszomor@organicawater.com>
2020-03-13 11:40:24 +0100

    init

--- CleanUpASCII/Program.cs
+++ CleanUpASCII/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace CleanUpASCII
 	{
 		static void Main(string[] args)
 		{
+			string file = "file.fil";
+			FileStream inputFile = new FileStream(file, FileMode.Open, FileAccess.ReadWrite);
+			Encoding e = Encoding.GetEncoding(1250);
+			char[] buffer = new char[1024];
+			int szam;
+			long p = 0;
+			using (StreamReader inputFileStreamRead = new StreamReader(inputFile, e))
+			{
+				StreamWriter fileToWrite = new StreamWriter(inputFile, e);
+				while((szam = inputFileStreamRead.Read(buffer, 0, 1024)) > 0)
+				{
+					for (int i = 0; i < szam; i++)
+					{
+						if (buffer[i] > 126)
+						{
+							buffer[i] = ' ';
+						}
+						else if (buffer[i] < 32)
+						{
+							buffer[i] = ' ';
+						}
+					}
+					inputFile.Position = p;
+					fileToWrite.WriteLine(buffer, 0, szam);
+					p = inputFile.Position;
+				}
+				fileToWrite.Close();
+			}
 		}
 	}
 }
